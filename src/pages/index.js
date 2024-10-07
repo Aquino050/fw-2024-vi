114b9165b4b261033832f5929a5a5c5897dc2091
Aquino050 <vitoria.aquino+github@estudante.ifms.edu.br>
2024-10-07 14:15:17 +0000

    Aula 07/10

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,7 @@ import Col from 'react-bootstrap/Col';
 export default function Index() {
   return <div>
     <Navbar />
-    Bem Vindos
-    <Container>
-      <Row>
-        <Col className="bg-primary">1 of 2</Col>
-        <Col className="bg-danger">2 of 2</Col>
-      </Row>
-      <Row>
-        <Col className="bg-info">1 of 3</Col>
-        <Col className="bg-dark text-white">2 of 3</Col>
-        <Col className="bg-warning ">3 of 3</Col>
-      </Row>
-    </Container>
+    Bem Vindos a disciplina
     <Footer />
   </div>;
 }
--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,4 +1,18 @@
-function Navbar() {
-    return<div>Menu FW</div>    
+import { Container, Nav, Navbar} from 'react-bootstrap';
+export default function Menu() {
+    return (
+        <Navbar expand="sm"  className="bg-dark">
+            <Container>
+                <Navbar.Brand href="/" className="text-light">6A</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="text-light" />
+                <Navbar.Collapse id="basic-navbar-nav" className="text-light">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className="text-light">Home</Nav.Link>
+                        <Nav.Link href="#link" className="text-light">Produtos</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
-export default Navbar;
+
