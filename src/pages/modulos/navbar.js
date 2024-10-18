ffdea02694c7559c3d80db116398fa28825aa270
Aquino050 <vitoria.aquino+github@estudante.ifms.edu.br>
2024-10-18 08:40:06 -0300

    copiado do Repositório da Aula

--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,20 +1,19 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import styles from "@/styles/Navbar.module.css"
 export default function Menu() {
-    return (
-        <Container>
-            <Navbar expand="sm" className={`bg-dark ${styles.navGradiente}  px-4 m-0`}>
-
-                <Navbar.Brand href="/" className="text-light">6A</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="text-light" />
-                <Navbar.Collapse id="basic-navbar-nav" className="text-light">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className="text-light">Home</Nav.Link>
-                        <Nav.Link href="#link" className="text-light">Produtos</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </Container>
-    );
+  return (
+    <Container>
+      <Navbar expand="sm" className={` bg-dark ${styles.navGradiente} navbar-dark px-3 m-0`}>
+        <Navbar.Brand href="/">6A</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Produtos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
+  );
 }
-
+// import styles from '@/styles/Navbar.module.css' {`${styles.navgradiente}
